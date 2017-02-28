d661cb18929ce1470e24bec27021b3ad273ca30d
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2017-03-01 00:43:49 +0100

    Make update scripts safer

--- bash/update-js.sh
+++ bash/update-js.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-cd ~/js
+js_dir="${HOME}/js"
 
-for d in */ ; do
-  ( cd "$d" && git pull && npm update && npm prune && npm outdated )
-done
+if [ -d "${js_dir}" ]; then
+    cd "${js_dir}"
+    for d in */ ; do
+        ( cd "$d" && git pull && npm update && npm prune && npm outdated )
+    done
+fi
