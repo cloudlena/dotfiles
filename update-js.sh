0a83ff077c4effe2294dca8821c293c3b3d5646a
Tobias Fuhrimann <tobias.fuhrimann@gmail.com>
2017-08-21 15:04:40 +0200

    Use npm dedupe

--- update-js.sh
+++ update-js.sh
@@ -8,6 +8,6 @@ js_dir="${HOME}/js"
 if [ -d "${js_dir}" ]; then
     cd "${js_dir}" || exit
     for d in */ ; do
-        ( cd "$d" && git pull && npm update && npm prune && npm outdated )
+        ( cd "$d" && git pull && npm update && npm prune && npm dedupe && npm outdated )
     done
 fi
