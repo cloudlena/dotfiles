ee5293205c0f91da508d32da4140bd152bb78d50
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2018-05-05 11:07:38 +0200

    Add awscli to Brewfile

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 # This script installs these dotfiles.
 
+set -e -u
+
 printf '\e[1mInitializing dotfiles\e[0m\n'
 
 case "$(uname)" in
@@ -13,7 +15,7 @@ case "$(uname)" in
     if ! xcode-select -p &> /dev/null; then
         read -p 'Xcode Command Line Tools not installed. You will have to run the script again after successfully installing them. Install now? (Y/n)' -r
         echo
-        if [[ ! $REPLY =~ ^[Nn]$ ]]
+        if [[ ! $REPLY =~ ^[Nn] ]]
         then
             xcode-select --install
             echo 'Please run the script again after the installation has finished'
@@ -24,7 +26,7 @@ case "$(uname)" in
     fi
 
     # Set dark mode
-    sudo defaults delete /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Dark
+    sudo defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Dark
 
     # Install Homebrew if not installed
     if [ ! -x "$(command -v brew)" ]; then
@@ -61,9 +63,15 @@ case "$(uname)" in
         stow --dir ~/dotfiles "$(basename "${dir}")"
     done
 
+    # Change npm folder
+    if [ -x "$(command -v npm)" ]; then
+        mkdir -p ~/.node_modules/lib
+        npm config set prefix ~/.node_modules
+    fi
+
     # Use vimrc as Neovim config
+    printf '\e[1mSetting up Neovim\e[0m\n'
     if [ -f ~/.config/nvim/init.vim ]; then
-        printf '\e[1mSetting up Neovim\e[0m\n'
         rm ~/.config/nvim/init.vim
     fi
     mkdir -p ~/.config/nvim
@@ -77,8 +85,8 @@ case "$(uname)" in
     pip install --upgrade --user neovim
 
     # Install vim-plug
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
     # Run full system upgrade
     source ~/dotfiles/bash/.path
@@ -102,10 +110,19 @@ case "$(uname)" in
         exit 1
     fi
 
+    # Set colors for pacman
+    sudo sed -i 's/#Color/Color/' /etc/pacman.conf
+
+    # Add Yaourt repo to pacman conf
+    grep -q -F "repo.archlinux.fr" /etc/pacman.conf || sudo sh -c 'echo '\''
+[archlinuxfr]
+SigLevel = Never
+Server = http://repo.archlinux.fr/$arch'\'' >> /etc/pacman.conf'
+
     # Install git if not installed
     if [ ! -x "$(command -v git)" ]; then
         printf '\e[1mInstalling Git\e[0m\n'
-        sudo pacman -Syu git --noconfirm
+        sudo pacman -Syu git --noconfirm --needed
     fi
 
     # git clone these dotfiles if not done yet
@@ -139,6 +156,8 @@ case "$(uname)" in
         python \
         python-pip \
         ruby \
+        aws-cli \
+        chromium \
         cmake \
         diff-so-fancy \
         docker \
@@ -161,22 +180,14 @@ case "$(uname)" in
         dep \
         delve \
         dropbox \
-        graphiql-app \
-        kubectl-bin \
-        minio-client \
         prezto-git \
         spotify \
         tmate
 
     # Enable docker service and allow user to run it without sudo
     sudo systemctl enable docker.service
-    sudo groupadd docker
+    getent group docker || groupadd docker
     sudo usermod -aG docker "${USER}"
-
-    # Change npm folder
-    if [ -x "$(command -v npm)" ]; then
-        npm config set prefix ~
-    fi
 
     # Use vimrc as Neovim config
     printf '\e[1mSetting up Neovim\e[0m\n'
@@ -190,8 +201,8 @@ case "$(uname)" in
     pip install neovim --upgrade --user
 
     # Install vim-plug
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
     # Run full system upgrade
     source ~/dotfiles/bash/.path
@@ -214,6 +225,7 @@ esac
 # Use zsh
 if [ -x "$(command -v zsh)" ]; then
     printf '\e[1mChanging your shell to zsh\e[0m\n'
+    grep -q -F "$(which zsh)" /etc/shells || sudo sh -c 'echo "$(which zsh)" >> /etc/shells'
     chsh -s "$(which zsh)"
 fi
 
