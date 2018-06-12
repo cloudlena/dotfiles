6a6ada02c8a3f83f624d58abb0dc573ee7210fff
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2018-06-12 20:42:26 +0200

    Run depu even if JS project is not a Git repo

--- update-js.sh
+++ update-js.sh
@@ -9,6 +9,6 @@ js_dir="${HOME}/js"
 if [ -d "${js_dir}" ]; then
     cd "${js_dir}" || exit
     for d in */ ; do
-        ( cd "$d" && git pull && depu )
+        ( cd "$d" && git pull ; depu )
     done
 fi
