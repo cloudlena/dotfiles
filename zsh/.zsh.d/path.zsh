1ef912484f3a31ff360c1b3b1392d2274f98e265
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2020-02-20 22:31:59 +0100

    Add date to polybar

--- zsh/.zsh.d/path.zsh
+++ zsh/.zsh.d/path.zsh
@@ -1,16 +1,16 @@
 PATH="/sbin"
-PATH="/bin:$PATH"
-PATH="/usr/sbin:$PATH"
-PATH="/usr/bin:$PATH"
-PATH="/usr/local/sbin:$PATH"
-PATH="/usr/local/bin:$PATH"
+PATH="/bin:${PATH}"
+PATH="/usr/sbin:${PATH}"
+PATH="/usr/bin:${PATH}"
+PATH="/usr/local/sbin:${PATH}"
+PATH="/usr/local/bin:${PATH}"
 
-PATH="$HOME/.node_modules/bin:$PATH"
+PATH="${HOME}/.node_modules/bin:${PATH}"
 
-PATH="$HOME/.cargo/bin:$PATH"
+PATH="${HOME}/.cargo/bin:${PATH}"
 
-GOPATH="$HOME/go"
-PATH="$GOPATH/bin:$PATH"
+GOPATH="${HOME}/go"
+PATH="${GOPATH}/bin:${PATH}"
 
 export GOPATH
 export PATH
