2ea25a98fa44aaeeba6f44de211ac09033abdf0d
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2024-02-10 21:21:01 +0100

    Improve Go binary update process

--- bash/update-go.sh
+++ bash/update-go.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-if [ -x "$(command -v binstale)" ]; then
-    for d in $(binstale | grep 'build ID mismatch'); do
-        ( if [[ $d == *'/'* ]]; then go get -u "$d"; fi )
-    done
+set -u
+
+if [ -x "$(command -v go)" ]; then
+    if [ ! -x "$(command -v binstale)" ]; then
+        go get -u github.com/shurcooL/binstale
+    fi
+
+    lines=$(binstale | grep 'stale: ')
+
+    while read -r line; do
+        if [ ! -z "${line}" ]; then
+            uri=$(echo "${line}" | awk -F ' ' '{print $2}')
+            echo "Updating Go binary ${uri}"
+            go get -u "${uri}"
+        fi
+    done < <(echo "${lines}")
 fi
--- bash/update-js.sh
+++ bash/update-js.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+set -u
+
 js_dir="${HOME}/js"
 
-if [ -d "${js_dir}" ]; then
-    cd "${js_dir}"
-    for d in */ ; do
-        ( cd "$d" && git pull && npm update && npm prune && npm outdated )
-    done
-fi
+cd "${js_dir}" || exit
+for d in */ ; do
+    ( cd "$d" && git pull && npm update && npm prune && npm outdated )
+done
