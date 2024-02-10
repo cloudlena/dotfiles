5513a1c62f000d5c3839902a85e78a26427be73e
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2024-02-10 21:31:59 +0100

    Add .txt files to spellcheck list

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -2,4 +2,4 @@
 vim.cmd("autocmd BufWritePre * %s/\\s\\+$//e")
 
 -- Enable spell checking for certain file types
-vim.cmd("autocmd BufRead,BufNewFile *.md,*.tex setlocal spell")
+vim.cmd("autocmd BufRead,BufNewFile *.txt,*.md,*.tex setlocal spell")
