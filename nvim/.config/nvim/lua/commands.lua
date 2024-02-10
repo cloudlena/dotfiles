008388e8b7ce699dcceb5163e3cfc695f7f6a119
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2024-02-10 21:31:24 +0100

    Ignore .gradle directory for quick jump

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -2,7 +2,7 @@ local opts = {noremap = true, silent = true}
 
 -- Toggle fuzzy search
 vim.api.nvim_set_keymap('n', '<C-P>',
-                        '<Cmd>lua require("telescope.builtin").find_files{ find_command = { "fd", "--type=f", "--type=l", "--hidden", "--no-ignore", "--exclude=.DS_Store", "--exclude=.git", "--exclude=.terraform", "--exclude=bin", "--exclude=build", "--exclude=coverage", "--exclude=dist", "--exclude=node_modules", "--exclude=target", "--exclude=vendor" }}<CR>',
+                        '<Cmd>lua require("telescope.builtin").find_files{ find_command = { "fd", "--type=f", "--type=l", "--hidden", "--no-ignore", "--exclude=.DS_Store", "--exclude=.git", "--exclude=.terraform", "--exclude=.gradle", "--exclude=bin", "--exclude=build", "--exclude=coverage", "--exclude=dist", "--exclude=node_modules", "--exclude=target", "--exclude=vendor" }}<CR>',
                         opts)
 vim.api.nvim_set_keymap('n', '\\', '<Cmd>Telescope live_grep<CR>', opts)
 -- vim.api.nvim_set_keymap('n', '\\', '<Cmd>lua require("telescope.builtin").grep_string{ search = vim.fn.input("Grep For > ") }<CR>', opts)
