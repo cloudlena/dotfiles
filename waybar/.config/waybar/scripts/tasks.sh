6223d0a885c80eb0fa57e09eae05d1799b075cb6
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2024-02-10 21:32:07 +0100

    Make tasks script for Waybar work with annotations

--- waybar/.config/waybar/scripts/tasks.sh
+++ waybar/.config/waybar/scripts/tasks.sh
@@ -6,16 +6,16 @@ if [ ! -x "$(command -v task)" ]; then
     exit 1
 fi
 
-active_task=$(task rc.verbose=nothing rc.report.activedesc.filter=+ACTIVE rc.report.activedesc.columns:description rc.report.activedesc.sort:urgency- rc.report.activedesc.columns:description activedesc limit:1)
+active_task=$(task rc.verbose=nothing rc.report.activedesc.filter=+ACTIVE rc.report.activedesc.columns:description rc.report.activedesc.sort:urgency- rc.report.activedesc.columns:description activedesc limit:1 | head -n 1)
 if [ -n "$active_task" ]; then
     echo " $active_task"
-    return 0
+    exit 0
 fi
 
-ready_task=$(task rc.verbose=nothing rc.report.readydesc.filter=+READY rc.report.readydesc.columns:description rc.report.readydesc.sort:urgency- rc.report.readydesc.columns:description readydesc limit:1)
+ready_task=$(task rc.verbose=nothing rc.report.readydesc.filter=+READY rc.report.readydesc.columns:description rc.report.readydesc.sort:urgency- rc.report.readydesc.columns:description readydesc limit:1 | head -n 1)
 if [ -z "$ready_task" ]; then
     echo ""
-    return 0
+    exit 0
 fi
 
 echo " $ready_task"
