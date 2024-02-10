f4ed3d5b163956928bebe696bb7536e325fc7c21
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2024-02-10 21:32:04 +0100

    Add taskwarrior to Waybar

---
+++ waybar/.config/waybar/scripts/tasks.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -u
+
+if [ ! -x "$(command -v task)" ]; then
+    exit 1
+fi
+
+active_task=$(task rc.verbose=nothing rc.report.activedesc.filter=+ACTIVE rc.report.activedesc.columns:description rc.report.activedesc.sort:urgency- rc.report.activedesc.columns:description activedesc limit:1)
+if [ -n "$active_task" ]; then
+    echo " $active_task"
+    return 0
+fi
+
+ready_task=$(task rc.verbose=nothing rc.report.readydesc.filter=+READY rc.report.readydesc.columns:description rc.report.readydesc.sort:urgency- rc.report.readydesc.columns:description readydesc limit:1)
+if [ -z "$ready_task" ]; then
+    echo ""
+    return 0
+fi
+
+echo " $ready_task"
