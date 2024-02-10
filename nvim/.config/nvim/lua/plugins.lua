d20c60bb391643d9b29801cfb177ca1b23f35d03
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2024-02-10 21:29:54 +0100

    Use standard code to install nvim-packer

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,12 +1,12 @@
 -- Install Packer if it's not installed yet
-local install_path = vim.fn.stdpath('data') ..
-                         '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.fn.system({
-        'git', 'clone', 'https://github.com/wbthomason/packer.nvim',
-        install_path
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+        'git', 'clone', '--depth', '1',
+        'https://github.com/wbthomason/packer.nvim', install_path
     })
-    vim.api.nvim_command('packadd packer.nvim')
+    vim.cmd 'packadd packer.nvim'
 end
 
 return require('packer').startup(function()
