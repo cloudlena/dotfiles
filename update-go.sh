b577fe27d15a32c373419faca8552f3e1de12264
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2024-02-10 21:26:15 +0100

    Use gobin instead of binstale

--- update-go.sh
+++ update-go.sh
@@ -3,17 +3,9 @@
 set -u
 
 if [ -x "$(command -v go)" ]; then
-    if [ ! -x "$(command -v binstale)" ]; then
-        go get -u github.com/shurcooL/binstale
+    if [ ! -x "$(command -v gobin)" ]; then
+        go get -u github.com/rjeczalik/bin/cmd/gobin
     fi
 
-    lines=$(binstale | grep 'stale: ')
-
-    while read -r line; do
-        if [ ! -z "${line}" ]; then
-            uri=$(echo "${line}" | awk -F ' ' '{print $2}')
-            echo "Updating Go binary ${uri}"
-            go get -u "${uri}"
-        fi
-    done < <(echo "${lines}")
+    gobin -u
 fi
