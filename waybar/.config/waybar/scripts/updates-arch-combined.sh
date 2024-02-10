a37d5e549c0540f8c65d3754815eab5d590f17d8
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2024-02-10 21:32:02 +0100

    Make updates script POSIX compliant

--- waybar/.config/waybar/scripts/updates-arch-combined.sh
+++ waybar/.config/waybar/scripts/updates-arch-combined.sh
@@ -8,7 +8,7 @@ if ! update_count_aur=$(paru -Qum 2>/dev/null | wc -l); then
     update_count_aur=0
 fi
 
-update_count=$(("$update_count_pacman" + "$update_count_aur"))
+update_count=$((update_count_pacman + update_count_aur))
 
 if [ "$update_count" -gt 0 ]; then
     echo "$update_count"
