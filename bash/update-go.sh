11adc2139badbe20b62d863b654287c36599e9b2
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2024-02-10 21:20:56 +0100

    Make update scripts safer

--- bash/update-go.sh
+++ bash/update-go.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for d in $(binstale | grep 'build ID mismatch'); do
-  ( if [[ $d == *'/'* ]]; then go get -u "$d"; fi )
-done
-
+if [ -x "$(command -v binstale)" ]; then
+    for d in $(binstale | grep 'build ID mismatch'); do
+        ( if [[ $d == *'/'* ]]; then go get -u "$d"; fi )
+    done
+fi
